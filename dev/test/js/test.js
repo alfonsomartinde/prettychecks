b5198a0ae44e6a7e9ee7f3945df8aff5bd9653bc
Alfonso Martín <alfonsomartin@gmail.com>
2013-12-13 09:49:20 +0100

    Ajusto web.js

--- dev/test/js/test.js
+++ dev/test/js/test.js
@@ -1,13 +1,10 @@
 $(function(){
 
-    $.fn.refresh = function() {
-        return $(this.selector);
-    };
-
     module( "Globals" );
     test( "Libraries loaded on global scope", function() {
         ok( typeof($ !== "undefined") , "jQuery loaded!" );
     });
+    /*
     module( "Radios" );
     test( "Parent <span> changes to 'checked' when click on <input>", function() {
 
@@ -16,11 +13,12 @@ $(function(){
 
         ok( $parent.hasClass("checked") === false , "<span> has not class='checked'..." );
         
-        $parent.trigger("click").refresh();
+        $parent.trigger("click");
         ok( $parent.hasClass("checked") === true , "... after click, <span> has now class='checked'... " );
 
-        $parent.trigger("click").refresh();
+        $parent.trigger("click");
         ok( $parent.hasClass("checked") === false , "... after one more click, <span> has not class='checked'" );
 
     });
+*/
 });
--- web.js
+++ web.js
@@ -1,6 +1,14 @@
+process.env.PWD = process.cwd();
 var express = require("express");
 var app = express();
 app.use(express.logger());
+
+app.configure(function() {
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.static(process.env.PWD + '/dev'));
+});
 
 app.get('/', function(request, response) {
     response.send('Server OK!');
