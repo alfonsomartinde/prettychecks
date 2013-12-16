cce7e15d42423355ec2c71ba223ce3e38f90a020
Alfonso Martín <alfonsomartin@gmail.com>
2013-12-16 18:34:01 +0100

    * Removed timeout from qunit:all task * Added phantomjs --version to travis.yml * Removed some brefore_scripts from travis.yml * Added grunt-cli to devDependencies

--- app/test/js/test.js
+++ app/test/js/test.js
@@ -1,24 +1,25 @@
-$(function(){
+if ( window.sessionStorage ) {
+        sessionStorage.clear();
+}
 
-    module( "Globals" );
-    test( "Libraries loaded on global scope", function() {
-        ok( typeof($ !== "undefined") , "jQuery loaded!" );
-    });
-    /*
-    module( "Radios" );
-    test( "Parent <span> changes to 'checked' when click on <input>", function() {
-
-        var $input = $( "#test2" ),
-            $parent = $input.parent();
-
-        ok( $parent.hasClass("checked") === false , "<span> has not class='checked'..." );
-        
-        $parent.trigger("click");
-        ok( $parent.hasClass("checked") === true , "... after click, <span> has now class='checked'... " );
-
-        $parent.trigger("click");
-        ok( $parent.hasClass("checked") === false , "... after one more click, <span> has not class='checked'" );
-
-    });
-*/
+module( "Globals" );
+test( "Libraries loaded on global scope", function() {
+    ok( typeof($ !== "undefined") , "jQuery loaded!" );
 });
+/*
+module( "Radios" );
+test( "Parent <span> changes to 'checked' when click on <input>", function() {
+
+    var $input = $( "#test2" ),
+        $parent = $input.parent();
+
+    ok( $parent.hasClass("checked") === false , "<span> has not class='checked'..." );
+    
+    $parent.trigger("click");
+    ok( $parent.hasClass("checked") === true , "... after click, <span> has now class='checked'... " );
+
+    $parent.trigger("click");
+    ok( $parent.hasClass("checked") === false , "... after one more click, <span> has not class='checked'" );
+
+});
+*/
